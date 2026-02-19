5238f46e3a31ddebed813abd291d02ed329f9287
maguilarsilva <mariana.sistemas@gmail.com>
2026-02-19 08:34:59 -0500

    Adicionar classes: orderItem e OrderItemPK e seus relacionamentos.

--- src/main/java/com/desuperior/dscommerce/entities/Order.java
+++ src/main/java/com/desuperior/dscommerce/entities/Order.java
@@ -3,6 +3,10 @@ package com.desuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_order")
@@ -20,6 +24,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order" , cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {}
 
@@ -70,4 +77,14 @@ public class Order {
     public void setPayment(Payment payment) {
         this.payment = payment;
     }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Product> getProducts() {
+        return items.stream().map(OrderItem::getProduct).collect(Collectors.toList());
+    }
+
+
 }
--- src/main/java/com/desuperior/dscommerce/entities/Product.java
+++ src/main/java/com/desuperior/dscommerce/entities/Product.java
@@ -3,7 +3,9 @@ package com.desuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_product")
@@ -23,6 +25,9 @@ public class Product {
             joinColumns = @JoinColumn(name = "product_id") ,
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){}
 
@@ -78,4 +83,11 @@ public class Product {
         return categories;
     }
 
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
+    public List<Order> getOders() {
+        return items.stream().map(OrderItem::getOrder).collect(Collectors.toList());
+    }
 }
