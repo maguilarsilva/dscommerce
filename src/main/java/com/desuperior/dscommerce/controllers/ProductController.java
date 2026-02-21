9576e4a2439a182b7ba219de4d10cf61cfea757d
maguilarsilva <mariana.sistemas@gmail.com>
2026-02-21 06:34:05 -0500

    Add API Products (atualizar)

--- src/main/java/com/desuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/desuperior/dscommerce/controllers/ProductController.java
@@ -2,7 +2,6 @@ package com.desuperior.dscommerce.controllers;
 
 import com.desuperior.dscommerce.dto.ProductDTO;
 import com.desuperior.dscommerce.services.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
